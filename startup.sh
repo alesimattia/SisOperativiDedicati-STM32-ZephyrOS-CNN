9d3074fffb1c7f9aa1d0c0ef5633da7e5e3846a8
Alesi Mattia <alesimattia@gmail.com>
2024-05-30 12:55:07 +0200

    Integrazione TensorFlow Micro

--- startup.sh
+++ startup.sh
@@ -8,24 +8,17 @@ diff ~/disconnect.txt ~/reconnect.txt
 
 readonly BOARD="stm32f429i_disc1"
 readonly PORT="tty.usbmodem1103"
-PROJECT="2_integrazione"
+PROJECT="1_progetto"
 
 # Virtual environment
 source ~/zephyrproject/.venv/bin/activate
 cd ~/zephyrproject/zephyr
 
 #Build (auto pristine)
-west build --pristine auto --board $BOARD $PROJECT
+west build --pristine always --board $BOARD $PROJECT
 west flash
 
 # Start Serial communication
 screen /dev/$PORT 115200
 # Press Reset and look!
 # CTRL-A then CTRL-\  to quit serial comm.
-
-
-
-#Add tflite-micro module to the West manifest 
-#(First time only)
-#west config manifest.project-filter -- +tflite-micro
-#west update
