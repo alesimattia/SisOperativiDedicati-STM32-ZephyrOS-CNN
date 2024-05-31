4ff073eb84ecb0a90a45c28c5d6cb9fd45a7d1b3
Alesi Mattia <alesimattia@gmail.com>
2024-05-31 13:54:48 +0200

    Backup

--- src/main.cc
+++ src/main.cc
@@ -1,17 +1,10 @@
-/* Copyright 2019 The TensorFlow Authors. All Rights Reserved.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
+/**
+----------------------------------------------------------------------
+@author: Alesi Mattia s1114418
+@project: CNN Person Detector su ZephyrOS
+@cite: TensorFlow Lite for Microcontroller
+----------------------------------------------------------------------
+**/
 
 #include "tensorflow/lite/micro/examples/person_detection/main_functions.h"
 
@@ -24,4 +17,5 @@ int main(int argc, char* argv[]) {
   while (true) {
     loop();
   }
+  return 0; //richiesto da specifiche C++ ZephyrOS
 }
--- startup.sh
+++ startup.sh
@@ -1,24 +1,24 @@
-# Discover board serial port assigned in your SO
+# Trova l'indirizzo della porta seriale assegnata dal S.O.
 cd /dev
-ls > ~/disconnect.txt
-# connect board
-ls > ~/reconnect.txt
-diff ~/disconnect.txt ~/reconnect.txt
-# something like tty.usbserial-DN014TJU
+ls > ~/without.txt
+# Connetti la board
+ls > ~/connected.txt
+diff ~/without.txt ~/connected.txt
+# qualcosa come tty.usbserial-DN014TJU
 
 readonly BOARD="stm32f429i_disc1"
 readonly PORT="tty.usbmodem1103"
 PROJECT="1_progetto"
 
-# Virtual environment
+# Attiva ambiente virtuale
 source ~/zephyrproject/.venv/bin/activate
 cd ~/zephyrproject/zephyr
 
 #Build (auto pristine)
-west build --pristine always --board $BOARD $PROJECT
+west build --pristine auto --board $BOARD $PROJECT
 west flash
 
-# Start Serial communication
+# Avvia monitor seriale
 screen /dev/$PORT 115200
-# Press Reset and look!
-# CTRL-A then CTRL-\  to quit serial comm.
+# Premi Reset sulla board!
+# CTRL-A then CTRL-\  per uscire dal monitor
